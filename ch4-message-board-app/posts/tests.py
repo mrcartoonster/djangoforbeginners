736b484fc0abb61de7c681c048ab32b374bf488f
Evan Baird <mrcartoonster@gmail.com>
2020-05-02 12:19:52 -0600

    📦 NEW: DT-1 converting ch4 tests to pytest

--- ch4-message-board-app/posts/tests.py
+++ ch4-message-board-app/posts/tests.py
@@ -1,5 +1,7 @@
+import pytest
 from django.test import TestCase
 from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
 
 from .models import Post
 
@@ -30,3 +32,31 @@ class HomePageViewTest(TestCase):
         resp = self.client.get(reverse('home'))
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'home.html')
+
+
+@pytest.mark.second
+def test_text_content(db):
+    Post.objects.create(text="just a test")
+    post = Post.objects.get(id=1)
+    expected_object_name = f"{post.text}"
+    assert expected_object_name == "just a test"
+
+
+@pytest.mark.third
+@pytest.mark.usefixtures("db", "client")
+class TestHomePageView:
+    def setUp(self, db):
+        Post.objects.create(text="this is another test")
+
+    def test_view_user_exists_at_proper_location(self, client):
+        resp = client.get("/")
+        assert resp.status_code == 200
+
+    def test_view_url_by_name(self, client):
+        resp = client.get(reverse("home"))
+        assert resp.status_code == 200
+
+    def test_view_uses_correct_template(self, client):
+        resp = client.get(reverse("home"))
+        assert resp.status_code == 200
+        assertTemplateUsed(resp, "home.html")
